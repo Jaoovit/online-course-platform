8eb0cfdcdba6ac7c261418947686a209b49156c0
João Oliveira <jaoovit@gmail.com>
2025-10-13 20:49:21 +0100

    initial project

---
+++ src/test/java/com/oliveira/online_course_platform/OnlineCoursePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveira.online_course_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCoursePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
