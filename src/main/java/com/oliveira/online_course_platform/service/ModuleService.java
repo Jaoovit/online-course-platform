fb568e58e4c274a500182cba1ef49d7a13224ba7
João Oliveira <jaoovit@gmail.com>
2025-11-08 10:03:46 +0000

    create Module method to create modules

--- src/main/java/com/oliveira/online_course_platform/service/ModuleService.java
+++ src/main/java/com/oliveira/online_course_platform/service/ModuleService.java
@@ -1,8 +1,13 @@
 package com.oliveira.online_course_platform.service;
 
+import com.oliveira.online_course_platform.domain.course.Course;
+import com.oliveira.online_course_platform.domain.module.Module;
+import com.oliveira.online_course_platform.domain.module.RequestModuleDTO;
 import com.oliveira.online_course_platform.repository.ModuleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class ModuleService {
@@ -11,5 +16,23 @@ public class ModuleService {
     private ModuleRepository moduleRepository;
 
     @Autowired
+    private ValidationService validationService;
+
+    @Autowired
     private CourseService courseService;
+
+    public Module createModule(RequestModuleDTO data, UUID courseId) {
+
+        validationService.validateInput(data);
+
+        Course course = courseService.getCourseById(courseId);
+
+        Module module = new Module();
+        module.setTitle(data.title());
+        module.setDescription(data.description());
+        module.setCourse(course);
+
+        moduleRepository.save(module);
+        return module;
+    }
 }
