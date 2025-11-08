6b4332e2bd4b0f1c9e775e102d436f24d7a4be2c
João Oliveira <jaoovit@gmail.com>
2025-11-08 09:32:51 +0000

    init module

---
+++ src/main/java/com/oliveira/online_course_platform/controller/ModuleController.java
@@ -0,0 +1,9 @@
+package com.oliveira.online_course_platform.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/module")
+public class ModuleController {
+}
---
+++ src/main/java/com/oliveira/online_course_platform/service/ModuleService.java
@@ -0,0 +1,12 @@
+package com.oliveira.online_course_platform.service;
+
+import com.oliveira.online_course_platform.repository.ModuleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ModuleService {
+
+    @Autowired
+    private ModuleRepository moduleRepository;
+}
