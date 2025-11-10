0a3e6e326297f9c67b07aaa9b3da4605f2b8a063
João Oliveira <jaoovit@gmail.com>
2025-11-10 21:07:36 +0000

    add transactional and modifying decorator to update queries

--- src/main/java/com/oliveira/online_course_platform/repository/LessonRepository.java
+++ src/main/java/com/oliveira/online_course_platform/repository/LessonRepository.java
@@ -2,8 +2,10 @@ package com.oliveira.online_course_platform.repository;
 
 import com.oliveira.online_course_platform.domain.lesson.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,6 +15,8 @@ public interface LessonRepository extends JpaRepository<Lesson, UUID> {
     @Query("SELECT e FROM Lesson e LEFT JOIN e.module a WHERE a.id = :moduleId")
     public List<Lesson> findLessonByModuleId(@Param("moduleId") UUID lessonId);
 
+    @Transactional
+    @Modifying
     @Query("UPDATE Lesson e SET e.title = :title, e.description = :description WHERE e.id = :lessonId")
     public int updateLesson(@Param("lessonId") UUID moduleId,
                                  @Param("title") String title,
--- src/main/java/com/oliveira/online_course_platform/repository/ModuleRepository.java
+++ src/main/java/com/oliveira/online_course_platform/repository/ModuleRepository.java
@@ -2,8 +2,10 @@ package com.oliveira.online_course_platform.repository;
 
 import com.oliveira.online_course_platform.domain.module.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,6 +15,8 @@ public interface ModuleRepository extends JpaRepository<Module, UUID> {
     @Query("SELECT e FROM Module e LEFT JOIN e.course a WHERE a.id = :courseId")
     public List<Module> findModuleByCourseId(@Param("courseId") UUID courseId);
 
+    @Transactional
+    @Modifying
     @Query("UPDATE Module e SET e.title = :title, e.description = :description WHERE e.id = :moduleId")
     public int updateModule(@Param("moduleId") UUID moduleId,
                                @Param("title") String title,
