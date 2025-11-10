5332c3382ab1bbae4a4af4977a6dd92556785319
João Oliveira <jaoovit@gmail.com>
2025-11-10 20:53:15 +0000

    create method to create lesson

--- src/main/java/com/oliveira/online_course_platform/service/LessonService.java
+++ src/main/java/com/oliveira/online_course_platform/service/LessonService.java
@@ -1,8 +1,13 @@
 package com.oliveira.online_course_platform.service;
 
+import com.oliveira.online_course_platform.domain.lesson.Lesson;
+import com.oliveira.online_course_platform.domain.lesson.RequestLessonDTO;
+import com.oliveira.online_course_platform.domain.module.Module;
 import com.oliveira.online_course_platform.repository.LessonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class LessonService {
@@ -15,4 +20,19 @@ public class LessonService {
 
     @Autowired
     private ModuleService moduleService;
+
+    public Lesson createLesson(RequestLessonDTO data, UUID moduleId) {
+
+        validationService.validateInput(data);
+
+        Module module = moduleService.getModuleById(moduleId);
+
+        Lesson lesson = new Lesson();
+        lesson.setTitle(data.title());
+        lesson.setDescription(data.description());
+        lesson.setModule(module);
+
+        lessonRepository.save(lesson);
+        return lesson;
+    }
 }
