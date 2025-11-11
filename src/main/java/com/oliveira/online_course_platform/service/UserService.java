116415c98ab118fc082cd16cc9de90e2a90d4d54
João Oliveira <jaoovit@gmail.com>
2025-11-11 20:59:32 +0000

    create method to get user by id

--- src/main/java/com/oliveira/online_course_platform/service/UserService.java
+++ src/main/java/com/oliveira/online_course_platform/service/UserService.java
@@ -2,15 +2,23 @@ package com.oliveira.online_course_platform.service;
 
 import com.oliveira.online_course_platform.domain.user.User;
 import com.oliveira.online_course_platform.exceptions.BadRequest;
+import com.oliveira.online_course_platform.exceptions.NotFound;
 import com.oliveira.online_course_platform.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    private User getUserById(UUID userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new NotFound("User not found"));
+    }
 
     protected void findUserByUserName(String username) {
         User user = userRepository.findByUsername(username);
