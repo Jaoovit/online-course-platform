c0c3ae42dfb84e9583c99193efb112f39a01c8f5
João Oliveira <jaoovit@gmail.com>
2025-11-13 20:44:50 +0000

    create controller to add course to user

--- src/main/java/com/oliveira/online_course_platform/controller/UserController.java
+++ src/main/java/com/oliveira/online_course_platform/controller/UserController.java
@@ -1,15 +1,35 @@
 package com.oliveira.online_course_platform.controller;
 
+import com.oliveira.online_course_platform.domain.user.ResponseUserDTO;
+import com.oliveira.online_course_platform.domain.user.User;
 import com.oliveira.online_course_platform.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @PostMapping("{userId}/{courseId}")
+    public ResponseEntity<ResponseUserDTO> addCourseToUser(@RequestParam UUID userId, @RequestParam UUID courseId) {
+        User user = userService.addCourseToUser(userId, courseId);
+        ResponseUserDTO userDTO = new ResponseUserDTO(
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getUsername(),
+                user.getCourses());
+        return ResponseEntity.ok(userDTO);
+    }
 
 }
--- src/main/java/com/oliveira/online_course_platform/domain/user/ResponseUserDTO.java
+++ src/main/java/com/oliveira/online_course_platform/domain/user/ResponseUserDTO.java
@@ -1,8 +1,13 @@
 package com.oliveira.online_course_platform.domain.user;
 
+import com.oliveira.online_course_platform.domain.course.Course;
+
+import java.util.List;
+
 public record ResponseUserDTO(String firstName,
                               String lastName,
                               String email,
-                              String username
+                              String username,
+                              List<Course> courses
                               ) {
 }
